7445e263d3cf988142c8f40b948b4472fb1fa770
akashdas-0111 <61954560+akashdas-0111@users.noreply.github.com>
2022-01-27 20:28:43 +0530

    Added files that is shows notification

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+from plyer import notification
+if __name__ == '__main__':
+    notification.notify(
+        title="Stand up",
+        message="sitting too much is not good for health",
+        timeout=5
+    )
+     
